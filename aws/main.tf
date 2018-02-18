cbc590123da1d92a4cc9bdc1f670a21da12d8ba7
ken5scal <kengoscal@gmail.com>
2018-02-18 18:56:00 +0900

    ASG and ELB

--- aws/main.tf
+++ aws/main.tf
@@ -16,10 +16,16 @@ data "aws_ami" "amazon_linxu2" {
   }
 }
 
+# each AWS account has access to a slightly diff set of AZs
+# so fetch the AZs specific to the AWS account
+data "aws_availability_zones" "all" {}
+
+# Specifies how to configure each EC2 instance in the ASG.
 resource "aws_launch_configuration" "ex-launch-config" {
   image_id        = "${var.image_id}"
   instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.example-sg.id}"]
+  key_name        = "${aws_key_pair.terraform-key.key_name}"
 
   user_data = <<-EOF
     #!/bin/bash
@@ -35,6 +41,44 @@ resource "aws_launch_configuration" "ex-launch-config" {
   }
 }
 
+# ASG itself
+resource "aws_autoscaling_group" "ex-asg" {
+  launch_configuration = "${aws_launch_configuration.ex-launch-config.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  load_balancers       = ["${aws_elb.ex-elb.name}"]
+  health_check_type    = "ELB"
+  min_size             = 2
+  max_size             = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# ELB
+resource "aws_elb" "ex-elb" {
+  name               = "terraform-elb-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.sg-elb.id}"]
+
+  listener {
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+  }
+}
+
 resource "aws_key_pair" "terraform-key" {
   key_name   = "terraform-key"
   public_key = "${file("${local.public_key_filename}")}"
@@ -43,7 +87,7 @@ resource "aws_key_pair" "terraform-key" {
 resource "aws_instance" "example" {
   # ami                    = "${data.aws_ami.amazon_linxu2.image_id}"
   ami                    = "${var.image_id}"
-  instance_type          = "t2.micro"
+  instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${aws_security_group.example-sg.id}"]
   key_name               = "${aws_key_pair.terraform-key.key_name}"
 
@@ -82,15 +126,26 @@ resource "aws_security_group" "example-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "sg-elb" {
+  name = "terraform-ex-elb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
   }
 }
 
@@ -109,6 +164,10 @@ variable "instance_type" {
 
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.ex-elb.dns_name}"
 }
 
 locals {
