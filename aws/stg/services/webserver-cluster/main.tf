a99ec353764f489cae27e084b7d25ae3cbef28e8
ken5scal <kengoscal@gmail.com>
2018-02-20 01:43:48 +0900

    moduled

--- aws/stg/services/webserver-cluster/main.tf
+++ aws/stg/services/webserver-cluster/main.tf
@@ -11,3 +11,13 @@ module "webserver_cluster" {
   instance_type = "t2.micro"
   min_size = "2"
 }
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  from_port = 12345
+  protocol = "tcp"
+  security_group_id = "${module.webserver_cluster.elb_security_group_id}"
+  to_port = 12345
+  type = "ingress"
+  cidr_blocks = [
+    "0.0.0.0/0"]
+}
